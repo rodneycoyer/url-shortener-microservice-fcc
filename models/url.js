0f39802ea99118f26162f2a3bae5491f4dbd1ceb
roofWalker <roofwalker13@outlook.com>
2021-08-30 14:22:50 -0700

    middlware setup

---
+++ models/url.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const URLshortSchema = new Schema({
+    original_url: {
+        type: String,
+        required: true,
+    },
+    short_url: {
+        type: Number,
+    }
+});
+
+const URLshort = mongoose.model('urlShort', URLshortSchema);
+
+module.exports = URLshort;
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const URLshort = require('./models/url');
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+// connect to mongo server with mongoose
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).catch(err => console.log(err.reason));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}));
+// parse application/json
+app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -18,6 +31,28 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+
+// url shortener API endpoint
+app.post('/api/shorturl/new', function (req, res) {
+  // user url entry
+  const userUrlInput = req.body.url;
+  // create but do not execute query search
+  const findUrl = URLshort.find({original_url: userUrlInput});
+
+
+  res.json({
+    original_url: userUrlInput,
+    short_url: ''
+  });
+  console.log(req.body)
+});
+
+
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
